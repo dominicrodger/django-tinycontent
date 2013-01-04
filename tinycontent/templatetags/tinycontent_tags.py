7e2f4c8949075eb1d11d2604da12ff0e35f4c60d
Dominic Rodger <dominicrodger+github@gmail.com>
2013-01-04 17:49:01 +0000

    Tests for the content name being a context variable

--- tinycontent/templatetags/tinycontent_tags.py
+++ tinycontent/templatetags/tinycontent_tags.py
@@ -8,12 +8,28 @@ register = template.Library()
 
 class TinyContentNode(template.Node):
     def __init__(self, content_name, nodelist):
-        self.content_name = content_name.strip("'")
+        self.content_name = content_name
         self.nodelist = nodelist
+
+    def get_content_name(self, context):
+        if self.content_name[0] != '"' and self.content_name[0] != "'":
+            try:
+                return context[self.content_name]
+            except KeyError:
+                return ''
+
+        if self.content_name[0] == '"' and self.content_name[-1] == '"':
+            return self.content_name[1:-1]
+
+        if self.content_name[0] == "'" and self.content_name[-1] == "'":
+            return self.content_name[1:-1]
+
+        return self.content_name
 
     def render(self, context):
         try:
-            obj = TinyContent.objects.get(name=self.content_name)
+            name = self.get_content_name(context)
+            obj = TinyContent.objects.get(name=name)
             return obj.content
         except TinyContent.DoesNotExist:
             return self.nodelist.render(context)
--- tinycontent/tests.py
+++ tinycontent/tests.py
@@ -5,7 +5,13 @@ import unittest
 
 def render_template(input):
     t = Template("{% load tinycontent_tags %}" + input)
-    c = Context({'meaning': 42})
+    c = Context()
+    return t.render(c)
+
+
+def render_template_with_context(input, context):
+    t = Template("{% load tinycontent_tags %}" + input)
+    c = Context(context)
     return t.render(c)
 
 
@@ -40,10 +46,59 @@ class TinyContentTestCase(unittest.TestCase):
         self.assertEqual("This should be found.",
                          render_template(t))
 
+    def testAlternateTextIfFoundDoubleQuotes(self):
+        TinyContent.objects.create(name='test_name_double',
+                                   content='This should be found.')
+
+        t = ('{% tinycontent "test_name_double" %}'
+             'I could not find it.'
+             '{% endtinycontent %}')
+
+        self.assertEqual("This should be found.",
+                         render_template(t))
+
     def testAlternateTextIfNotFoundSupportsOtherTags(self):
         t = ("{% tinycontent 'neverexists' %}"
              "I could not find {{ meaning }}."
              "{% endtinycontent %}")
 
+        ctx = {'meaning': 42}
+
         self.assertEqual("I could not find 42.",
+                         render_template_with_context(t, ctx))
+
+    def testAllowsContextVariablesAsContentName(self):
+        TinyContent.objects.create(name='variable_name',
+                                   content='This is a variable test.')
+        t = ("{% tinycontent_simple content_name %}")
+
+        ctx = {'content_name': 'variable_name'}
+
+        self.assertEqual("This is a variable test.",
+                         render_template_with_context(t, ctx))
+
+    def testAllowsContextVariablesAsContentNameFromComplex(self):
+        TinyContent.objects.create(name='another_variable',
+                                   content='This is another variable test.')
+        t = ("{% tinycontent content_name %}"
+             "Text if empty."
+             "{% endtinycontent %}")
+
+        ctx = {'content_name': 'another_variable'}
+
+        self.assertEqual("This is another variable test.",
+                         render_template_with_context(t, ctx))
+
+    def testAllowsUnprovidedContextVariablesAsContentNameFromComplex(self):
+        t = ("{% tinycontent content_name %}"
+             "Text if empty."
+             "{% endtinycontent %}")
+
+        self.assertEqual("Text if empty.",
+                         render_template(t))
+
+    def testAllowsUnprovidedContextVariablesAsContentNameFromSimple(self):
+        t = ("{% tinycontent_simple content_name %}")
+
+        self.assertEqual("",
                          render_template(t))
