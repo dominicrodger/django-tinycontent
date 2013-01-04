d36af3b834324a702c01f512099fde7a39bba78c
Dominic Rodger <dominicrodger+github@gmail.com>
2013-01-04 17:10:58 +0000

    Adding a more complex tag which handles non-existent blocks

--- tinycontent/templatetags/tinycontent_tags.py
+++ tinycontent/templatetags/tinycontent_tags.py
@@ -1,12 +1,41 @@
 from django import template
+from django.template.base import TemplateSyntaxError
 from tinycontent.models import TinyContent
 
 
 register = template.Library()
 
 
+class TinyContentNode(template.Node):
+    def __init__(self, content_name, nodelist):
+        self.content_name = content_name.strip("'")
+        self.nodelist = nodelist
+
+    def render(self, context):
+        try:
+            obj = TinyContent.objects.get(name=self.content_name)
+            return obj.content
+        except TinyContent.DoesNotExist:
+            return self.nodelist.render(context)
+
+
+@register.tag
+def tinycontent(parser, token):
+    args = token.split_contents()
+
+    if len(args) != 2:
+        raise TemplateSyntaxError("'tinycontent' tag takes exactly one"
+                                  " argument.")
+
+    content_name = args[1]
+
+    nodelist = parser.parse(('endtinycontent',))
+    parser.delete_first_token()
+    return TinyContentNode(content_name, nodelist)
+
+
 @register.simple_tag
-def tinycontent(name):
+def tinycontent_simple(name):
     try:
         obj = TinyContent.objects.get(name=name)
         return obj.content
--- tinycontent/tests.py
+++ tinycontent/tests.py
@@ -5,18 +5,45 @@ import unittest
 
 def render_template(input):
     t = Template("{% load tinycontent_tags %}" + input)
-    c = Context()
+    c = Context({'meaning': 42})
     return t.render(c)
 
 
 class TinyContentTestCase(unittest.TestCase):
     def testNonExistent(self):
         self.assertEqual("",
-                         render_template("{% tinycontent 'foobar' %}"))
+                         render_template("{% tinycontent_simple 'foobar' %}"))
 
     def testSimpleExistent(self):
         TinyContent.objects.create(name='foobar',
                                    content='This is a test.')
 
         self.assertEqual("This is a test.",
-                         render_template("{% tinycontent 'foobar' %}"))
+                         render_template("{% tinycontent_simple 'foobar' %}"))
+
+    def testAlternateTextIfNotFound(self):
+        t = ("{% tinycontent 'neverexists' %}"
+             "I could not find it."
+             "{% endtinycontent %}")
+
+        self.assertEqual("I could not find it.",
+                         render_template(t))
+
+    def testAlternateTextIfFound(self):
+        TinyContent.objects.create(name='test_name',
+                                   content='This should be found.')
+
+        t = ("{% tinycontent 'test_name' %}"
+             "I could not find it."
+             "{% endtinycontent %}")
+
+        self.assertEqual("This should be found.",
+                         render_template(t))
+
+    def testAlternateTextIfNotFoundSupportsOtherTags(self):
+        t = ("{% tinycontent 'neverexists' %}"
+             "I could not find {{ meaning }}."
+             "{% endtinycontent %}")
+
+        self.assertEqual("I could not find 42.",
+                         render_template(t))
