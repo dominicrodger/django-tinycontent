204e608d7968033876e4010ad83d3d6cb2645152
Dominic Rodger <dominicrodger+github@gmail.com>
2013-01-05 08:40:18 +0000

    Make content blocks searchable in the admin

--- tinycontent/admin.py
+++ tinycontent/admin.py
@@ -4,5 +4,6 @@ from tinycontent.models import TinyContent
 
 class TinyContentAdmin(admin.ModelAdmin):
     list_display = ('name', )
+    search_fields = ('name', 'content', )
 
 admin.site.register(TinyContent, TinyContentAdmin)
