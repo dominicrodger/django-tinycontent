8191e0facbf1c1a5326a3bb902691a8ec587bdc2
Dominic Rodger <dominicrodger+github@gmail.com>
2015-06-02 17:22:09 +0100

    Move some more fixture work into fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,19 +4,47 @@ from __future__ import absolute_import
 
 import pytest
 from tinycontent.models import TinyContent
+from django.contrib.auth.models import User, Permission
 
 
 @pytest.fixture()
 def simple_content():
-    return TinyContent.objects.get_or_create(
+    content, _ = TinyContent.objects.get_or_create(
         name='foobar',
         content='This is a test.'
     )
+    return content
 
 
 @pytest.fixture()
 def html_content():
-    return TinyContent.objects.get_or_create(
+    content, _ = TinyContent.objects.get_or_create(
         name='html',
         content='<strong>&amp;</strong>'
     )
+    return content
+
+
+@pytest.fixture()
+def user():
+    user, _ = User.objects.get_or_create(username='dom')
+
+    add_perm = Permission.objects.get(
+        codename='add_tinycontent'
+    )
+    change_perm = Permission.objects.get(
+        codename='change_tinycontent'
+    )
+
+    user.user_permissions.add(add_perm)
+    user.user_permissions.add(change_perm)
+    user.save()
+
+    return user
+
+
+@pytest.fixture()
+def user_noauth():
+    user, _ = User.objects.get_or_create(username='barry')
+
+    return user
--- tests/test_tinycontent.py
+++ tests/test_tinycontent.py
@@ -5,7 +5,6 @@ import sys
 # Needed for the custom filter tests
 sys.path.append(os.path.dirname(__file__))
 
-from django.contrib.auth.models import User, Permission
 from django.contrib.auth.context_processors import PermWrapper
 from django.core.exceptions import ImproperlyConfigured
 from django.core.urlresolvers import reverse
@@ -27,8 +26,7 @@ def render_template_with_context(input, context):
     return t.render(c).strip()
 
 
-def render_for_test_user(t):
-    user = User.objects.get(username='dom')
+def render_for_test_user(t, user):
     ctx = {'user': user, 'perms': PermWrapper(user), }
     return render_template_with_context(t, ctx)
 
@@ -188,57 +186,45 @@ def test_bad_arguments(simple_content):
 
 
 @pytest.mark.django_db
-def test_with_user(simple_content):
-    user, is_new_user = User.objects.get_or_create(username='dom')
-
+def test_with_user(simple_content, user, user_noauth):
     t = ("{% tinycontent 'foobar' %}"
          "Text if empty."
          "{% endtinycontent %}")
 
-    assert "This is a test." == render_for_test_user(t)
-
-    perm = Permission.objects.get(codename='change_tinycontent')
-    user.user_permissions.add(perm)
-    user.save()
+    assert "This is a test." == render_for_test_user(t, user_noauth)
 
     root_edit_url = reverse('admin:tinycontent_tinycontent_change',
-                            args=[1, ])
+                            args=[simple_content.pk, ])
 
-    rendered = render_for_test_user(t)
+    rendered = render_for_test_user(t, user)
     assert root_edit_url in rendered
     assert 'Edit' in rendered
     assert "This is a test." in rendered
 
     t = "{% tinycontent_simple 'foobar' %}"
-    rendered = render_for_test_user(t)
+    rendered = render_for_test_user(t, user)
     assert root_edit_url in rendered
     assert 'Edit' in rendered
     assert "This is a test." in rendered
 
 
 @pytest.mark.django_db
-def test_with_user_for_nonexistent_tag(simple_content):
-    user, is_new_user = User.objects.get_or_create(username='dom')
-
+def test_with_user_for_nonexistent_tag(simple_content, user, user_noauth):
     t = ("{% tinycontent 'notthere' %}"
          "Text if empty."
          "{% endtinycontent %}")
 
-    assert "Text if empty." == render_for_test_user(t)
-
-    perm = Permission.objects.get(codename='add_tinycontent')
-    user.user_permissions.add(perm)
-    user.save()
+    assert "Text if empty." == render_for_test_user(t, user_noauth)
 
     root_add_url = reverse('admin:tinycontent_tinycontent_add')
 
-    rendered = render_for_test_user(t)
+    rendered = render_for_test_user(t, user)
     assert '%s?name=notthere' % root_add_url in rendered
     assert 'Add' in rendered
     assert "Text if empty." in rendered
 
     t = "{% tinycontent_simple 'notthere' %}"
-    rendered = render_for_test_user(t)
+    rendered = render_for_test_user(t, user)
     assert '%s?name=notthere' % root_add_url in rendered
     assert 'Add' in rendered
 
