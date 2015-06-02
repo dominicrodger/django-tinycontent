d84a47b875af42da3491c771e461b0a8ca5556db
Dominic Rodger <dominicrodger+github@gmail.com>
2015-06-02 22:38:36 +0100

    Test the slug field is generated correctly

--- tests/test_models.py
+++ tests/test_models.py
@@ -9,3 +9,8 @@ def test_tinycontent_str(simple_content):
 @pytest.mark.django_db
 def test_tinycontentfile_str(file_upload):
     assert "Foobar" == str(file_upload)
+
+
+@pytest.mark.django_db
+def test_tinycontentfile_slug(file_upload):
+    assert "foobar" == file_upload.slug
