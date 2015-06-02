dd827ba226746e2ff6e830685c595c20d79b28ac
Dominic Rodger <dominicrodger+github@gmail.com>
2015-06-02 17:32:17 +0100

    Split out tests for the content name sources

---
+++ tests/test_content_name.py
@@ -0,0 +1,84 @@
+import pytest
+
+from .utils import (
+    render_template,
+    render_template_with_context
+)
+
+
+@pytest.mark.django_db
+def test_allows_context_variables_as_content_names_from_simple(simple_content):
+    t = ("{% tinycontent_simple content_name %}")
+
+    ctx = {'content_name': 'foobar'}
+
+    assert "This is a test." == render_template_with_context(t, ctx)
+
+
+@pytest.mark.django_db
+def test_allows_context_variables_as_content_names_from_complex(
+        simple_content
+):
+    t = ("{% tinycontent content_name %}"
+         "Text if empty."
+         "{% endtinycontent %}")
+
+    ctx = {'content_name': 'foobar'}
+
+    assert "This is a test." == render_template_with_context(t, ctx)
+
+
+@pytest.mark.django_db
+def test_allows_with_tag_as_content_names_from_simple(simple_content):
+    t = ("{% with content_name='foobar' %}"
+         "{% tinycontent_simple content_name %}"
+         "{% endwith %}")
+
+    assert "This is a test." == render_template(t)
+
+
+@pytest.mark.django_db
+def test_allows_with_tag_as_content_names_from_complex(simple_content):
+    t = ("{% with content_name='foobar' %}"
+         "{% tinycontent content_name %}"
+         "Text if empty."
+         "{% endtinycontent %}"
+         "{% endwith %}")
+
+    assert "This is a test." == render_template(t)
+
+
+@pytest.mark.django_db
+def test_allows_unprovided_ctx_variables_as_content_name_complex(
+        simple_content
+):
+    t = ("{% tinycontent content_name %}"
+         "Text if empty."
+         "{% endtinycontent %}")
+
+    assert "Text if empty." == render_template(t)
+
+
+@pytest.mark.django_db
+def test_allows_unprovided_ctx_variables_as_content_name_simple(
+        simple_content
+):
+    t = ("{% tinycontent_simple content_name %}")
+
+    assert "" == render_template(t)
+
+
+@pytest.mark.django_db
+def test_ctx_variables_with_name_of_content_complex(simple_content):
+    t = ("{% tinycontent foobar %}"
+         "Text if empty."
+         "{% endtinycontent %}")
+
+    assert "Text if empty." == render_template(t)
+
+
+@pytest.mark.django_db
+def test_ctx_variables_with_name_of_content_simple(simple_content):
+    t = ("{% tinycontent_simple foobar %}")
+
+    assert "" == render_template(t)
--- tests/test_tinycontent.py
+++ tests/test_tinycontent.py
@@ -3,10 +3,7 @@ import pytest
 from django.template.base import TemplateSyntaxError
 
 from tinycontent.models import TinyContent
-from .utils import (
-    render_template,
-    render_template_with_context
-)
+from .utils import render_template
 
 
 @pytest.mark.django_db
@@ -24,84 +21,6 @@ def test_simple_existent(simple_content):
     assert "This is a test." == render_template(
         "{% tinycontent_simple 'foobar' %}"
     )
-
-
-@pytest.mark.django_db
-def test_allows_context_variables_as_content_names_from_simple(simple_content):
-    t = ("{% tinycontent_simple content_name %}")
-
-    ctx = {'content_name': 'foobar'}
-
-    assert "This is a test." == render_template_with_context(t, ctx)
-
-
-@pytest.mark.django_db
-def test_allows_context_variables_as_content_names_from_complex(
-        simple_content
-):
-    t = ("{% tinycontent content_name %}"
-         "Text if empty."
-         "{% endtinycontent %}")
-
-    ctx = {'content_name': 'foobar'}
-
-    assert "This is a test." == render_template_with_context(t, ctx)
-
-
-@pytest.mark.django_db
-def test_allows_with_tag_as_content_names_from_simple(simple_content):
-    t = ("{% with content_name='foobar' %}"
-         "{% tinycontent_simple content_name %}"
-         "{% endwith %}")
-
-    assert "This is a test." == render_template(t)
-
-
-@pytest.mark.django_db
-def test_allows_with_tag_as_content_names_from_complex(simple_content):
-    t = ("{% with content_name='foobar' %}"
-         "{% tinycontent content_name %}"
-         "Text if empty."
-         "{% endtinycontent %}"
-         "{% endwith %}")
-
-    assert "This is a test." == render_template(t)
-
-
-@pytest.mark.django_db
-def test_allows_unprovided_ctx_variables_as_content_name_complex(
-        simple_content
-):
-    t = ("{% tinycontent content_name %}"
-         "Text if empty."
-         "{% endtinycontent %}")
-
-    assert "Text if empty." == render_template(t)
-
-
-@pytest.mark.django_db
-def test_allows_unprovided_ctx_variables_as_content_name_simple(
-        simple_content
-):
-    t = ("{% tinycontent_simple content_name %}")
-
-    assert "" == render_template(t)
-
-
-@pytest.mark.django_db
-def test_ctx_variables_with_name_of_content_complex(simple_content):
-    t = ("{% tinycontent foobar %}"
-         "Text if empty."
-         "{% endtinycontent %}")
-
-    assert "Text if empty." == render_template(t)
-
-
-@pytest.mark.django_db
-def test_ctx_variables_with_name_of_content_simple(simple_content):
-    t = ("{% tinycontent_simple foobar %}")
-
-    assert "" == render_template(t)
 
 
 @pytest.mark.django_db
