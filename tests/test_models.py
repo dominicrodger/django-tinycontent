0fb1a4e7b53320b8b1687cd42aa7d13f511e0983
Dominic Rodger <dominicrodger+github@gmail.com>
2015-06-02 22:27:08 +0100

    Avoid re-fetching objects we already have

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,8 +1,6 @@
 import pytest
 
-from tinycontent.models import TinyContent
-
 
 @pytest.mark.django_db
-def test_str(simple_content):
-    assert "foobar" == str(TinyContent.objects.get(name='foobar'))
+def test_tinycontent_str(simple_content):
+    assert "foobar" == str(simple_content)
