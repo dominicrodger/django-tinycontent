8ff4b1d2dded7f50a9c48788b9ffd72fa03ce26d
Dominic Rodger <dominicrodger+github@gmail.com>
2015-06-03 12:39:39 +0100

    It's a filter, not a renderer

--- tests/test_md_filter.py
+++ tests/test_md_filter.py
@@ -4,12 +4,12 @@ from tinycontent.models import TinyContent
 from .utils import render_template
 
 
-RENDERER = 'tinycontent.filters.md.markdown_filter'
+TEST_FILTER = 'tinycontent.filters.md.markdown_filter'
 
 
 @pytest.mark.django_db
 def test_with_markdown_filter_simple(simple_content, settings):
-    settings.TINYCONTENT_FILTER = RENDERER
+    settings.TINYCONTENT_FILTER = TEST_FILTER
     assert "<p>This is a test.</p>" == render_template(
         "{% tinycontent_simple 'foobar' %}"
     )
@@ -17,7 +17,7 @@ def test_with_markdown_filter_simple(simple_content, settings):
 
 @pytest.mark.django_db
 def test_with_markdown_filter_newline(settings):
-    settings.TINYCONTENT_FILTER = RENDERER
+    settings.TINYCONTENT_FILTER = TEST_FILTER
 
     TinyContent.objects.get_or_create(
         name='newline',
