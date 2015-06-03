043e91c53b11879a5bef19fec3710be1b06a5041
Dominic Rodger <dominicrodger+github@gmail.com>
2015-06-03 08:07:00 +0100

    Noting Python 3.4 support
    Python 3.3 is untested via tox at this point, but I'm not
    too concerned about that.

--- setup.py
+++ setup.py
@@ -42,6 +42,7 @@ setup(
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.3",
+        "Programming Language :: Python :: 3.4",
     ],
     tests_require=(
         "pytest==2.7.1",
