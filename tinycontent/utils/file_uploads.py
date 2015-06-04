9ef11f52f069ad1a59aa5c68620d0f9dd80cc955
Dominic Rodger <dominicrodger+github@gmail.com>
2015-06-04 20:25:10 +0100

    Pull out the code for matching file paths, so other filters can re-use

---
+++ tinycontent/utils/file_uploads.py
@@ -0,0 +1,26 @@
+import re
+from tinycontent.models import TinyContentFileUpload
+
+
+fileupload_expression = re.compile(r'(@file:([A-Za-z0-9\-_]+))')
+
+
+class FileUploadMatch(object):
+    def __init__(self, match):
+        self.full = match[0]
+        self.file = TinyContentFileUpload.objects.get(
+            slug=match[1]
+        )
+
+
+def get_fileuploads(text):
+    fileuploads = []
+
+    for match in fileupload_expression.findall(text):
+        try:
+            fileuploads.append(FileUploadMatch(match))
+        except TinyContentFileUpload.DoesNotExist:
+            # Just ignore bad slugs
+            pass
+
+    return fileuploads
