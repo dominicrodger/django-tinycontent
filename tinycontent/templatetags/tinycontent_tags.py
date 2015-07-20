397b127119dff28b85f736eeec3d4aba067ee095
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-07-20 23:39:16 +0200

    Proposal support for template tags

--- tinycontent/templatetags/tinycontent_tags.py
+++ tinycontent/templatetags/tinycontent_tags.py
@@ -1,8 +1,6 @@
 from django import template
-from django.template.base import TemplateSyntaxError
 from django.template.loader import render_to_string
 from tinycontent.models import TinyContent
-
 
 register = template.Library()
 
@@ -14,28 +12,22 @@ class TinyContentNode(template.Node):
 
     def render(self, context):
         try:
-            name = self.content_name.resolve(context)
-            obj = TinyContent.objects.get(name=name)
+            obj = TinyContent.objects.get(name=self.content_name)
             return render_to_string('tinycontent/tinycontent.html',
                                     {'obj': obj},
                                     context)
         except TinyContent.DoesNotExist:
             rval = self.nodelist.render(context)
             rval += render_to_string('tinycontent/tinycontent_add.html',
-                                     {'name': name},
+                                     {'name': self.content_name},
                                      context)
             return rval
 
 
 @register.tag
 def tinycontent(parser, token):
-    args = token.split_contents()
-
-    if len(args) != 2:
-        raise TemplateSyntaxError("'tinycontent' tag takes exactly one"
-                                  " argument.")
-
-    content_name = parser.compile_filter(args[1])
+    args = token.split_contents()[1:]
+    content_name = ':'.join(map(unicode, args))
 
     nodelist = parser.parse(('endtinycontent',))
     parser.delete_first_token()
@@ -43,13 +35,14 @@ def tinycontent(parser, token):
 
 
 @register.simple_tag(takes_context=True)
-def tinycontent_simple(context, name):
+def tinycontent_simple(context, *args):
+    content_name = ':'.join(map(unicode, args))
     try:
-        obj = TinyContent.objects.get(name=name)
+        obj = TinyContent.objects.get(name=content_name)
         return render_to_string('tinycontent/tinycontent.html',
                                 {'obj': obj},
                                 context)
     except TinyContent.DoesNotExist:
         return render_to_string('tinycontent/tinycontent_add.html',
-                                {'name': name},
+                                {'name': content_name},
                                 context)
