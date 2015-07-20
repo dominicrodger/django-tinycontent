a441346f5b2090d99ae6d59b2b5838f1413b7fce
Adam Dobrawy <naczelnik@jawnosc.tk>
2015-07-21 00:03:05 +0200

    Fix backward compatibility

--- tinycontent/templatetags/tinycontent_tags.py
+++ tinycontent/templatetags/tinycontent_tags.py
@@ -6,32 +6,34 @@ register = template.Library()
 
 
 class TinyContentNode(template.Node):
-    def __init__(self, content_name, nodelist):
-        self.content_name = content_name
+    def __init__(self, args, nodelist):
+        self.args = args
         self.nodelist = nodelist
+
+    def get_name(self, context):
+        return ':'.join(x.resolve(context) for x in self.args)
 
     def render(self, context):
         try:
-            obj = TinyContent.objects.get(name=self.content_name)
+            name = self.get_name(context)
+            obj = TinyContent.objects.get(name=name)
             return render_to_string('tinycontent/tinycontent.html',
                                     {'obj': obj},
                                     context)
         except TinyContent.DoesNotExist:
             rval = self.nodelist.render(context)
             rval += render_to_string('tinycontent/tinycontent_add.html',
-                                     {'name': self.content_name},
+                                     {'name': name},
                                      context)
             return rval
 
 
 @register.tag
 def tinycontent(parser, token):
-    args = token.split_contents()[1:]
-    content_name = ':'.join(map(unicode, args))
-
+    args = [parser.compile_filter(x) for x in token.split_contents()[1:]]
     nodelist = parser.parse(('endtinycontent',))
     parser.delete_first_token()
-    return TinyContentNode(content_name, nodelist)
+    return TinyContentNode(args, nodelist)
 
 
 @register.simple_tag(takes_context=True)
