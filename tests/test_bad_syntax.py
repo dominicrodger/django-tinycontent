abe5a491d7a64f19f08c472e83abf2317493df94
Dominic Rodger <dominicrodger+github@gmail.com>
2015-07-23 08:10:48 +0100

    Get test coverage back up to 100%

--- tests/test_bad_syntax.py
+++ tests/test_bad_syntax.py
@@ -14,6 +14,14 @@ def test_wrong_number_of_arguments(simple_content):
 
 
 @pytest.mark.django_db
+def test_wrong_number_of_arguments_simple(simple_content):
+    t = ("{% tinycontent_simple %}")
+
+    with pytest.raises(TemplateSyntaxError):
+        render_template(t)
+
+
+@pytest.mark.django_db
 def test_bad_arguments(simple_content):
     t = ("{% tinycontent 'foo %}{% endtinycontent %}")
     with pytest.raises(TemplateSyntaxError):
